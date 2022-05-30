94ef4d5de77e4e23085c3b993aa42f2d6f4e14ef
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-05-29 18:15:17 -0600

    use ints

--- src/Nomics.hs
+++ src/Nomics.hs
@@ -48,8 +48,8 @@ fetchAssets = do
   response <-
     HTTP.httpJSONEither $ HTTP.setRequestQueryString
       [ ("key"     , Just . T.encodeUtf8 $ nomicsApiKey)
-      , ("page"    , Just . T.encodeUtf8 $ "1")
-      , ("per-page", Just . T.encodeUtf8 $ "5")
+      , ("page"    , Just . T.encodeUtf8 $ tshow 1)
+      , ("per-page", Just . T.encodeUtf8 $ tshow 100)
       , ("interval", Just . T.encodeUtf8 $ "1d")
       , ("status"  , Just . T.encodeUtf8 $ "active")
       , ("convert" , Just . T.encodeUtf8 $ "usd")
