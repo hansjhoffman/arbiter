0e158b89b8923f98bc0c8ec84a7851310cca87bc
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-05-30 18:06:32 -0600

    improve options parser

--- app/Main.hs
+++ app/Main.hs
@@ -13,30 +13,31 @@ import           Run
 import           System.Environment             ( getEnv )
 
 
-optionsParser :: Parser Options
-optionsParser = Options <$> switch (long "verbose" <> short 'v' <> help "Verbose output?")
+optionsParser :: IO (Options, ())
+optionsParser = do
+  simpleOptions
+    $(simpleVersion Paths_arbiter.version)
+    "Header for command line arguments"
+    "Fetch all active crypto assets from the Nomics API and send them to Algolia for search."
+    (Options <$> switch (long "verbose" <> short 'v' <> help "Verbose output?"))
+    empty
 
 
 main :: IO ()
 main = do
-  (options, ()) <- simpleOptions
-    $(simpleVersion Paths_arbiter.version)
-    "Header for command line arguments"
-    "Program description, also for command line arguments"
-    optionsParser
-    empty
-  logOpts <- logOptionsHandle stderr (optionsVerbose options)
-  processCtx <- mkDefaultProcessContext
+  (options, ()) <- optionsParser
+  logOpts       <- logOptionsHandle stderr (optionsVerbose options)
+  processCtx    <- mkDefaultProcessContext
   algoliaApiKey <- getEnv "RYKA_ALGOLIA_API_KEY"
-  algoliaAppId <- getEnv "RYKA_ALGOLIA_APP_ID"
-  algoliaIndex <- getEnv "RYKA_ALGOLIA_INDEX"
-  nomicsApiKey <- getEnv "NOMICS_API_KEY"
+  algoliaAppId  <- getEnv "RYKA_ALGOLIA_APP_ID"
+  algoliaIndex  <- getEnv "RYKA_ALGOLIA_INDEX"
+  nomicsApiKey  <- getEnv "NOMICS_API_KEY"
   withLogFunc logOpts $ \logFn ->
-    let app = App { appAlgoliaApiKey = T.pack algoliaApiKey
-                  , appAlgoliaAppId = T.pack algoliaAppId
-                  , appAlgoliaIndex = T.pack algoliaIndex
-                  , appLogFn        = logFn
-                  , appNomicsApiKey = T.pack nomicsApiKey
+    let app = App { appAlgoliaApiKey  = T.pack algoliaApiKey
+                  , appAlgoliaAppId   = T.pack algoliaAppId
+                  , appAlgoliaIndex   = T.pack algoliaIndex
+                  , appLogFn          = logFn
+                  , appNomicsApiKey   = T.pack nomicsApiKey
                   , appOptions        = options
                   , appProcessContext = processCtx
                   }
