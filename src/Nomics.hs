b9c8cdd01aff098e8d29812b2f54a29611e5a767
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-05-31 19:03:44 -0600

    use types instead of strings

--- src/Nomics.hs
+++ src/Nomics.hs
@@ -29,6 +29,47 @@ instance FromJSON Crypto where
     Crypto <$> (obj .: "id") <*> (obj .: "logo_url") <*> (obj .: "name") <*> (obj .: "symbol")
 
 
+data Status
+  = Active
+  | Dead
+  | Inactive
+  deriving (Eq, Show)
+
+
+data Interval
+  = Day
+  | Hour
+  | Week
+  | Month
+  | Year
+  | YTD
+  deriving (Eq, Show)
+
+
+data Fiat = USD
+
+
+encodeStatus :: Status -> ByteString
+encodeStatus = T.encodeUtf8 . \case
+  Active   -> "active"
+  Dead     -> "dead"
+  Inactive -> "inactive"
+
+
+encodeInterval :: Interval -> ByteString
+encodeInterval = T.encodeUtf8 . \case
+  Day   -> "1d"
+  Hour  -> "1h"
+  Week  -> "7d"
+  Month -> "30d"
+  Year  -> "365d"
+  YTD   -> "ytd"
+
+
+encodeFiat :: Fiat -> ByteString
+encodeFiat USD = T.encodeUtf8 "usd"
+
+
 request :: Request
 request =
   HTTP.setRequestHost host
@@ -40,6 +81,10 @@ request =
   path = "v1/currencies/ticker"
 
 
+perPage :: Integer
+perPage = 100
+
+
 -- Nomics API has a rate limit of 1 request per second w/ free API keys. Need to debounce.
 fetchAssets :: RIO App (Either JSONException [Crypto])
 fetchAssets = do
@@ -49,10 +94,10 @@ fetchAssets = do
     HTTP.httpJSONEither $ HTTP.setRequestQueryString
       [ ("key"     , Just . T.encodeUtf8 $ nomicsApiKey)
       , ("page"    , Just . T.encodeUtf8 $ tshow (1 :: Integer))
-      , ("per-page", Just . T.encodeUtf8 $ tshow (100 :: Integer))
-      , ("interval", Just . T.encodeUtf8 $ "1d")
-      , ("status"  , Just . T.encodeUtf8 $ "active")
-      , ("convert" , Just . T.encodeUtf8 $ "usd")
+      , ("per-page", Just . T.encodeUtf8 $ tshow perPage)
+      , ("interval", Just $ encodeInterval Day)
+      , ("status"  , Just $ encodeStatus Active)
+      , ("convert" , Just $ encodeFiat USD)
       ]
       request :: RIO App (Response (Either JSONException [Crypto]))
   let totalItems = HTTP.getResponseHeader "X-Pagination-Total-Items" response
@@ -61,25 +106,12 @@ fetchAssets = do
   pure $ HTTP.getResponseBody response
 
 
--- fetchAssets' :: RIO App [Crypto]
--- fetchAssets' = fetchAssets'' 1 <$> ask
---  where
---   fetchAssets'' :: Int -> App -> RIO App (Either JSONException [Crypto])
---   fetchAssets'' pageNumber env = do
---     let nomicsApiKey = view nomicsApiKeyL env
---     response <-
---       HTTP.httpJSONEither $ HTTP.setRequestQueryString
---         [ ("key"     , Just . T.encodeUtf8 $ nomicsApiKey)
---         , ("page"    , Just . T.encodeUtf8 $ tshow pageNumber)
---         , ("per-page", Just . T.encodeUtf8 $ "5")
---         , ("interval", Just . T.encodeUtf8 $ "1d")
---         , ("status"  , Just . T.encodeUtf8 $ "active")
---         , ("convert" , Just . T.encodeUtf8 $ "usd")
---         ]
---         request :: RIO App (Response (Either JSONException [Crypto]))
---     case response of
---       Left  _ -> []
---       Right _ -> []
---     logInfo "Got page " <> show pageNumber
---     let totalItems = HTTP.getResponseHeader "X-Pagination-Total-Items" response
---     -- if totalItems / 100 (roundUp) > pageNumber then body ++ recursive_call else done
+-- google http conduit exception handling
+-- use some sort of "state monad" for pageNum and [Crypto]? Or just pass in as params?
+-- conduit fold? https://hackage.haskell.org/package/conduit-1.3.4.2/docs/Data-Conduit-Combinators.html#v:fold
+-- conduit repeatWhileM? https://hackage.haskell.org/package/conduit-1.3.4.2/docs/Data-Conduit-Combinators.html#v:repeatWhileM
+
+-- 1. fetch assets from Nomics API
+-- 2. grab totalCount from headers and determine if we've reached the end ==> totalItems / 100
+-- (round-up) > currentPageNumber
+-- 3. append assets to previous and repeat loop from step 1
